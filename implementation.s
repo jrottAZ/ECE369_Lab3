051c0d824fb2bfd76e3e7fd177e85e07e12418b2
bigpapa132525 <159649867+bigpapa132525@users.noreply.github.com>
2025-09-24 14:51:15 -0700

    Update implementation.s

--- implementation.s
+++ implementation.s
@@ -1,19 +1,30 @@
+.data 
 arr:	.word 1,  2,  6,  7,  15,
-        3,  5,  8,  14,  16,
-        4, 9, 13, 17, 22,
-        10, 12, 18, 21, 23,
-        11, 19, 20, 24, 25
+         3,  5,  8,  14,  16,
+         4, 9, 13, 17, 22,
+         10, 12, 18, 21, 23,
+         11, 19, 20, 24, 25
 fileX: .word 5
 fileY: .word 5        
 #arr = $a0
-#fileX = $a1
-#fileY = $a2
-#windowX = $a3
-#windowY = $a4
+#fileX = $s1
+#fileY = $s2
+#windowX = $s3
+#windowY = $s4
+
+
+# data for printing
+space:   .asciiz " "
+newline: .asciiz "\n"
+
+.text
+.globl Main
 Main: 
-	la $a0, arr
-	lw $a1, fileX
-	lw $a2, fileY
+	la $s0, arr
+	lw $s1, fileX
+	lw $s2, fileY
+	li $s3, 2
+	li $s4, 2
 VBSME:
 	#initialization
 	add $t0, $t0, $0 #x = 0
@@ -23,11 +34,11 @@ VBSME:
 	#finding xMax and yMax
 	#xMax = $t3
 	#yMax = $t4
-	subi $t3, $a3, 1
-	sub $t3, $a1, $t3
+	subi $t3, $s3, 1
+	sub $t3, $s1, $t3
 	
-	subi $t4, $a4, 1
-	sub $t4, $a2, $t4
+	subi $t4, $s4, 1
+	sub $t4, $s2, $t4
 	
 	#get xMax-1 and yMax-1 before loop
 	subi $t7, $t3, 1 #get xMax-1
@@ -46,8 +57,16 @@ VBSME:
 loop:	
 	beq $t5, $t6, exit   #exit when i = xMax*yMax
 	#if(dir == 1)
-	bne $t2, 1, dirfalse #go to dirfalse if dir != 1
 	
+	
+	#calculate area of array
+	mul $t9, $t1, $s1    # t9 = y * fileX
+	add $t9, $t9, $t0    # + x
+	sll $t9, $t9, 2      # *4 (bytes per word)
+	add $t9, $s0, $t9    # base + offset
+	lw  $t9, 0($t9)      # load arr[y][x] 
+	
+	bne $t2, 1, dirfalse #go to dirfalse if dir != 1
 	dirtrue:
 		#Check that we dont hit right boundary
 		#find if (x == xMax - 1) && (y != yMax - 1))
@@ -57,6 +76,7 @@ loop:
 		addi $t1, $t1, 1
 		add $t2, $0, $0
 		addi $t5, $t5, 1
+		jal print
 		j loop
 		
 		
@@ -68,6 +88,7 @@ loop:
 		addi $t0, $t0, 1
 		add $t2, $0, $0
 		addi $t5, $t5, 1
+		jal print
 		j loop
 		
 		
@@ -76,6 +97,7 @@ loop:
 		addi $t0, $t0, 1
 		addi $t1, $t1, -1
 		addi $t5, $t5, 1
+		jal print
 		j loop
 		 
 		
@@ -87,6 +109,7 @@ loop:
 		addi $t1, $t1, 1
 		addi $t2, $t2, 1
 		addi $t5, $t5, 1
+		jal print
 		j loop
 		
 		
@@ -96,6 +119,7 @@ loop:
 		addi $t0, $t0, 1
 		addi $t2, $t2, 1
 		addi $t5, $t5, 1
+		jal print
 		j loop
 		
 		nonehit:
@@ -103,12 +127,35 @@ loop:
 		addi $t0, $t0, -1
 		addi $t1, $t1, 1
 		addi $t5, $t5, 1
+		jal print
 		j loop
 exit:
 	jr $ra
 	
 	
 	
-print:
+print:	
+	# print the array value (in $t9)
+    li $v0, 1
+    move $a0, $t9
+    syscall
+
+    # print a space
+    li $v0, 4
+    la $a0, space
+    syscall
+
+    # print loop counter i ($t5)
+    li $v0, 1
+    move $a0, $t5
+    syscall
+
+    # print newline
+    li $v0, 4
+    la $a0, newline
+    syscall
+
+    jr $ra
+
 		
 	
